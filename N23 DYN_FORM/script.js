c8cfeeb752fc548c77327efda64e1394cb1c19b9
anna-gt <hannah.ivanovich@gmail.com>
2023-10-14 02:27:46 +0300

    refactor

--- N23 DYN_FORM/script.js
+++ N23 DYN_FORM/script.js
@@ -4,32 +4,60 @@ function createForm(arr) {
     const line = arr[i];
     let labelName = document.createTextNode(line.label);
     form1.appendChild(labelName);
-    // document.body.appendChild(p1);
-   // let input1 = document.createElement("input");
     let type1;
     let elem;
     if (line.kind === 'longtext' || line.kind === 'number' || line.kind === 'shorttext') {
       elem = 'input';
       type1 = 'text';
     }
-    else if (line.kind === 'combo')
+    else if (line.kind === 'combo') {
       elem = 'select';
+    }
     else if (line.kind === 'radio') {
       elem = 'input';
       type1 = 'radio';
     }
     else if (line.kind === 'check') {
-      elem = 'checkbox';
+      elem = 'input';
+      type1 = 'checkbox';
     }
     else if (line.kind === 'memo') 
       elem = 'textarea';
-      else if (line.kind === 'submit') {
+    else if (line.kind === 'submit') {
         elem = 'input';
         type1 = 'submit'; 
+      };
+    /* if (type1 === 'radio') {
+      for (let i = 0; i < line.variants.length; i++) {
+        let variant = line.variants[i];
+        let elem2 = document.createElement(elem);
+        let value1 = variant.value;
+        let radioName = variant.text;
+        let radio1 = document.createTextNode(radioName);
+        elem2.setAttribute("value", value1);
+        elem2.appendChild(radio1);
+    }
+  } */
+      let elem1 = document.createElement(elem);
+    if (elem === 'select') {
+      for (let i = 0; i < line.variants.length; i++){
+        let variant = line.variants[i];
+        let optionName = variant.text;
+        let value1 = variant.value;
+        let option1 = document.createElement('option');
+        let optionValue = document.createTextNode(optionName);
+        option1.appendChild(optionValue);
+        option1.setAttribute("value", value1);
+        elem1.appendChild(option1);
       }
-    let elem1 = document.createElement(elem);
+    };
+    if (type1 === 'submit') {
+      value1 = line.caption;
+      elem1.setAttribute("value", value1);
+    };
     elem1.setAttribute("type", type1);
     elem1.setAttribute("name", line.name);
+    elem1.setAttribute("checked", "checked");
     let newLine = document.createElement("br");
     form1.appendChild(elem1);
     form1.appendChild(newLine);
@@ -61,3 +89,4 @@ const formDef2=
   {caption:'Зарегистрироваться',kind:'submit'},
 ];
 createForm(formDef1);
+createForm(formDef2);
