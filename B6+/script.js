90d7a3857bd0dc2f49e7b0d7da7666122e537776
anna-gt <hannah.ivanovich@gmail.com>
2023-10-20 15:48:26 +0300

    fix

--- B6+/script.js
+++ B6+/script.js
@@ -1,4 +1,4 @@
-function buildWrapper(tag, string, atr) { 
+function buildWrapper(tag) { 
   function makeMnemonic(str) {
     let finStr = '';
     for (let i = 0; i < str.length; i++) {
@@ -17,16 +17,22 @@ function buildWrapper(tag, string, atr) {
     }
     return finStr;
   }
-  finString = makeMnemonic(string);
-  let finAtr = '';
-  for (let key in atr) {
-    if (key === 'title')
+  function wrapX (string, atr) {
+    finString = makeMnemonic(string);
+    let finAtr = '';
+    for (let key in atr) {
       atr[key] = makeMnemonic(atr[key]);
-    finAtr += ` ${key}='${atr[key]}'`;
+      finAtr += ` ${key}='${atr[key]}'`;
+    }
+    let result = `<${tag}${finAtr}> ${finString}<${tag}>`;
+    return result;
   }
-  let result = `<${tag}${finAtr}> ${finString}<${tag}>`;
-  return result;
+  return wrapX;
 }
 
-console.log(buildWrapper('P', "Однажды в <студёную> зимнюю пору",{lang:"ru"}));
-console.log(buildWrapper('H1', "СТИХИ",{align:"center",title:"M&M's"}));
+var wrapP=buildWrapper("P");  // строим функцию для оборачивания текста в тег P
+console.log( wrapP("Однажды в студёную зимнюю пору") );
+console.log( wrapP("Однажды в студёную зимнюю пору",{lang:"ru"}) );
+console.log( wrapP("Однажды в <студёную> зимнюю пору") );
+var wrapH1=buildWrapper("H1"); // строим функцию для оборачивания текста в тег H1
+console.log( wrapH1("СТИХИ",{align:"center",title:"M&M's"}) );
