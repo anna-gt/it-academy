04a4a3ded701e19a63dcdd340b5b9064ab18a7e8
anna-gt <hannah.ivanovich@gmail.com>
2023-10-31 20:33:17 +0300

    add scalable values

--- N33 CLOCK_DOM/script.js
+++ N33 CLOCK_DOM/script.js
@@ -6,21 +6,30 @@ const hours = 12; // количество часов на циферблате -
 const minsSec = 60; // количество минут и секунд на циферблате
 const partHour = 2*Math.PI/hours; // размер угла, равного одному часу
 const partMinsSec = 2*Math.PI/minsSec; // размер угла, равного одной минуте или секунде
+const corpColor = '#f0f092'; // цвет корпуса часов
+const numColor = '#6ca86c'; // цвет ячеек для цифр
+
 builderBttn.onclick = buildMyClock;
 
 function buildMyClock(eo) {
   const clockSize = parseInt(inputElem.value);
   const radius = clockSize/2;
-  const innerRadius = radius/10*8; // расстояние от центра часов до цифер
+  const innerRadius = radius/10*8; // расстояние от центра часов до цифр
   const hourArrLength = radius/2; // длина часовой стрелки
   const minArrLength = radius/4*3; // длина минутной стрелки
   const secArrLength = radius*0.9; // длина секундной стрелки
   const numbersCircleSize = clockSize/10; // размер полей с цифрами 
   const originBottomPoint = radius/15; // точка вращения стрелок 
+  const digClockSize = radius/5; // размер цифровых часов
+  const numsSize = radius/10; // размер цифр на стрелочных часах
+  const hoursArrowW = radius/25; // толщина часовой стрелки
+  const minsArrowW = radius/50; // толщина минутной стрелки
+  const secArrowW = radius/150; // толщина секундной стрелки
+
   clock.innerHTML = '';
   clock.style.width = clockSize + 'px';
   clock.style.height = clockSize + 'px';
-  clock.style.backgroundColor = '#f0f092';
+  clock.style.backgroundColor = corpColor;
   clock.style.borderRadius = 50 + '%';
   clock.style.position = 'relative';
   for (let i = 1; i <= hours; i++) {
@@ -28,7 +37,7 @@ function buildMyClock(eo) {
     clock.appendChild(numberCicrcle);
     numberCicrcle.style.width = numbersCircleSize + 'px';
     numberCicrcle.style.height = numbersCircleSize + 'px';
-    numberCicrcle.style.backgroundColor = '#6ca86c';
+    numberCicrcle.style.backgroundColor = numColor;
     numberCicrcle.style.borderRadius = 50 + '%';
     numberCicrcle.style.position = 'absolute';
     const angle = i*partHour;
@@ -43,7 +52,7 @@ function buildMyClock(eo) {
     numberElem.style.left = 50 + '%';
     numberElem.style.top = 50 + '%';
     numberElem.style.transform = 'translate(-50%,-50%)';
-    numberElem.style.fontSize = 2 + 'em';
+    numberElem.style.fontSize = numsSize + 'px';
     let number = document.createTextNode(i);
     numberElem.appendChild(number);
     numberCicrcle.appendChild(numberElem);
@@ -54,7 +63,7 @@ function buildMyClock(eo) {
   digTimeField.style.left = 50 + '%';
   digTimeField.style.top = 30 + '%';
   digTimeField.style.transform = 'translate(-50%,-50%)';
-  digTimeField.style.fontSize = 3 + 'em';
+  digTimeField.style.fontSize = digClockSize + 'px';
   updateTime();
   setInterval(updateTime,1000);
   function updateTime() {
@@ -80,7 +89,7 @@ function buildMyClock(eo) {
   const hourArr = document.createElement('div');
   clock.appendChild(hourArr);
   hourArr.style.position = 'absolute';
-  hourArr.style.width = 0.7 +'em'; // толщина часовой стрелки
+  hourArr.style.width = hoursArrowW + 'px'; // толщина часовой стрелки
   hourArr.style.height = hourArrLength + 'px'; 
   hourArr.style.left = 50 + '%';
   hourArr.style.top = (radius - hourArrLength) + 'px'; // top позиция стрелки = радиус - длина стрелки
@@ -105,7 +114,7 @@ function buildMyClock(eo) {
   const minArr = document.createElement('div');
   clock.appendChild(minArr);
   minArr.style.position = 'absolute';
-  minArr.style.width = 0.3 +'em'; // толщина минутной стрелки
+  minArr.style.width = minsArrowW +'px'; // толщина минутной стрелки
   minArr.style.height = minArrLength + 'px'; 
   minArr.style.left = 50 + '%';
   minArr.style.top = (radius - minArrLength) + 'px'; // top позиция стрелки = радиус - длина стрелки
@@ -127,7 +136,7 @@ function buildMyClock(eo) {
   const secArr = document.createElement('div');
   clock.appendChild(secArr);
   secArr.style.position = 'absolute';
-  secArr.style.width = 0.1 +'em'; // толщина секундной стрелки
+  secArr.style.width = secArrowW +'px'; // толщина секундной стрелки
   secArr.style.height = secArrLength + 'px'; 
   secArr.style.left = 50 + '%';
   secArr.style.top = (radius - secArrLength) + 'px'; // top позиция стрелки = радиус - длина стрелки
