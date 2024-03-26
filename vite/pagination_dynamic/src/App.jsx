5d631c7e3368f4fdf60cdf6ecfb109f78f0411e3
Anna <hannah.ivanovich@gmail.com>
2024-03-26 13:48:43 +0300

    use axios

--- vite/pagination_dynamic/src/App.jsx
+++ vite/pagination_dynamic/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import useSWR from "swr";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 
@@ -9,33 +9,19 @@ function App() {
 	const [fetching, setFetching] = useState(true);
 	const [totalCount, setTotalCount] = useState(0);
 
-	async function dataFetcher() {
-		const response = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${currentPage}`, {
-			method: 'get',
-			headers: {
-				"Accept": "application/json",
-			},
-		});
-		if (!response.ok) {
-			throw new Error("fetch error " + response.status);
-		}
-		return response.json();
-	}
-
-	const { data, error, isLoading } = useSWR(
-		"jsonphotos",
-		dataFetcher
-	);
-
 	useEffect(() => {
 		if (fetching) {
 			console.log('fetching');
-			if ( !isLoading ) {
-				setPhotos([...photos,...data]);
-		 }
-		 setCurrentPage(prevState => prevState + 1);
-		}	
-},[fetching, data, isLoading])
+			axios.get(`https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${currentPage}`)
+				.then(response => {
+					setPhotos([...photos,...response.data]);
+					setCurrentPage(prevState => prevState + 1);
+					setTotalCount(response.headers['x-total-count'])
+				})
+				.catch(error => console.log('error'))
+				.finally(() => setFetching(false))
+		}
+	}, [fetching])
 
 
 	useEffect(() =>{
@@ -43,21 +29,14 @@ function App() {
 		return function () {
 			document.removeEventListener('scroll', scrollHandler)
 		}
-	}, [totalCount, fetching]);
+	}, [totalCount]);
 
 	const scrollHandler = (eo) => {
-		if (eo.target.documentElement.scrollHeight - (eo.target.documentElement.scrollTop + window.innerHeight) < 100) {
+		if (eo.target.documentElement.scrollHeight - (eo.target.documentElement.scrollTop + window.innerHeight) < 100 
+		&& photos.length < totalCount) {
 			setFetching(true);
 		}
 	};
-
-	if ( error ) {
-    return <div>Ошибка: {error}</div>;
-  }
-
-  if ( isLoading ) {
-    return <div>загрузка данных...</div>;
-  }
 
 	const photoCode = photos.map(photo =>
 		<div className='photo' key={photo.id}>
