76a8efd3fc85e2541db0b4e06d2dfde5bd744498
Anna <hannah.ivanovich@gmail.com>
2024-04-12 16:45:05 +0300

    add app tests

--- vite/my-react-app/src/App.jsx
+++ vite/my-react-app/src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+	const [data, setData] = useState(null);
+	const [toggle, setToggle] = useState(false);
+	const [value, setValue] = useState('');
+
+	const onClick = () => setToggle(prev => !prev);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setData({})
+		}, 100)
+	}, [])
 
   return (
     <>
+			<h1 data-testid='value-elem'>{value}</h1>
+			{toggle === true && <div data-testid='toggle-elem'>toggle</div>}
+			{data && <div>data</div>}
       Hello world
+			<button data-testid='toggle-btn' onClick={onClick}>click me</button>
+			<input onChange={eo => setValue(eo.target.value)} type='text' placeholder='input value...'></input>
     </>
   )
 }
